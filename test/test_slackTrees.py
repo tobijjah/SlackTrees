0d311a70c55febebdd689cde93041c522367329f
tobijjah <tobias.seydewitz@posteo.net>
2017-11-30 21:29:31 +0100

    Add test for unfiltered_features

--- test/test_slackTrees.py
+++ test/test_slackTrees.py
@@ -1,6 +1,7 @@
+import os
 import unittest
 from slack_trees import SlackTrees
-from utilities import get_qgis_app
+from utilities import get_qgis_app, make_test_layer, delete_layer
 
 
 QGIS_APP, CANVAS, IFACE, PARENT = get_qgis_app()
@@ -10,12 +11,19 @@ class SlackTreesTest(unittest.TestCase):
 
     def setUp(self):
         self.plugin = SlackTrees(IFACE)
+        self.layer, self.path = make_test_layer(os.path.join(os.path.dirname(__file__), 'test_layer.shp'))
+        self.plugin.layer = self.layer
 
     def tearDown(self):
         self.plugin = None
+        self.layer = None
+        delete_layer(self.path)
 
-    def test_fail(self):
-        self.fail('FAIl')
+    def test_get_unfiltered_features(self):
+        expected_length = len(list(self.layer.getFeatures()))
+        actual_length = len(list(self.plugin._get_unfiltered_features()))
+
+        self.assertEqual(actual_length, expected_length)
 
 
 if __name__ == "__main__":
